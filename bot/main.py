db97cef4ee6b35e6b7598afe2437a588ab9ae279
Dylan Sclater <dylancsclater@gmail.com>
2021-08-31 18:34:57 -0400

    testing

--- bot/main.py
+++ bot/main.py
@@ -12,9 +12,14 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    message_text = message.content.strip().upper()
-    if keyword in message_text:
-        await bot.send_message(message.channel, "it worked")
+    await bot.send_message(message.channel, "it worked")
+
+
+# @bot.event
+# async def on_message(message):
+#     message_text = message.content.strip().upper()
+#     if keyword in message_text:
+#         await bot.send_message(message.channel, "it worked")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
