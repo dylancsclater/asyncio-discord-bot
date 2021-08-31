706f3c9509a0f5f0d6ede9bd560e5f2a9ba91ed0
Dylan Sclater <dylancsclater@gmail.com>
2021-08-31 19:48:26 -0400

    edited file name

--- bot/main.py
+++ bot/main.py
@@ -4,7 +4,6 @@ from discord import Client
 bot = Client()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-keyword = "SUPERIOR"
 
 @bot.event
 async def on_ready():
@@ -13,16 +12,9 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.content.contains("superior"):
-        with open('image.png', 'rb') as fh:
-            image = discord.File(fh)
-            await message.channel.send(file=image)
+        with open('image.png', 'rb') as fp:
+            await channel.send(file=discord.File(fp, 'new_image.png'))
 
-
-# @bot.event
-# async def on_message(message):
-#     message_text = message.content.strip().upper()
-#     if keyword in message_text:
-#         await bot.send_message(message.channel, "it worked")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
--- bot/test.py
+++
@@ -1,5 +0,0 @@
-import os
-print(os.listdir())
-
-with open('image.png', 'rb') as fh:
-    print("good")
