0e6e0040c0899026792564ff9dfcf6c6bae7ed76
Dylan Sclater <dylancsclater@gmail.com>
2021-08-31 18:08:01 -0400

    add await

--- bot/main.py
+++ bot/main.py
@@ -3,15 +3,17 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
 TOKEN = os.getenv("DISCORD_TOKEN")
+keyword = "SUPERIOR"
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
-@bot.listen()
+@bot.event()
 async def on_message(message):
-    if message.content.startswith("Superior"):
-	message.channel.send("Dylan Sclater is the Superior Dylan")
+        message_text = message.content.strip().upper()
+	if keyword in message_text
+		await bot.send_message(message.channel, "it worked")
 
 if __name__ == "__main__":
     bot.run(TOKEN)
