f28fbe24fdf30bac009a69a60a66ed64d02dc7c5
Dylan Sclater <dylancsclater@gmail.com>
2021-08-31 20:01:27 -0400

    removed parenthesis

--- bot/main.py
+++ bot/main.py
@@ -12,7 +12,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.content.contains("superior"):
-        await channel.send("All-time Win Record: Dylan Sclater: 4 vs Dylan Feeney:0"))
+        await channel.send("All-time Win Record: Dylan Sclater: 4 vs Dylan Feeney: 0")
 
 
 if __name__ == "__main__":
