959db26d80b7c140a4cd3c228c1cc4268460f139
Dylan Sclater <dylancsclater@gmail.com>
2021-09-02 08:47:59 -0400

    updated image and file from local bot server

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,23 @@
 import os
+import subprocess
 import discord
 
 bot = discord.Client()
-TOKEN = os.getenv("DISCORD_TOKEN")
+# TOKEN = os.getenv("DISCORD_TOKEN")
+
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
+
 @bot.event
 async def on_message(message):
-    if message.content.startswith("superior"):
+    """ When a message comes in, pauses the event loop, checks the message, if its superior, return the shit talk"""
+    if "superior" in (message.content.lower()):
         await message.channel.send("The all time record is Dylan Sclater: 4 Dylan Feeney: 0")
+        await message.channel.send(file=discord.File('image.png'))
 
 
 if __name__ == "__main__":
-    bot.run(TOKEN)
+    bot.run("ODgyMDI0MzQxNjAzMjM3OTU4.YS1XLg.lwViWNDOw64-7OROz6AfSr5z2eo")
